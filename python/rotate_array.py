8b46bd496b02c101ccea4536de30a1dbef52b71d
Amine El Hafidi <aminel-h@c3r9p9.1337.ma>
2026-02-17 22:45:10 +0000

    Solve rotate array problem

--- python/rotate_array.py
+++ python/rotate_array.py
@@ -4,20 +4,10 @@ from typing import List
 
 
 class Solution:
-    def copy(self, nums: List[int]):
-        i = len(nums) - 1
-        while i > 0:
-            nums[i] = nums[i - 1]
-            i -= 1
-
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i: int = 0
-        l: int = len(nums)
-        while i < k:
-            c: int = nums[l - 1]
-            self.copy(nums)
-            nums[0] = c
-            i += 1
+        part1 = nums[k:]
+        part2 = nums[:len(nums) - k]
+        nums[:] = part2 + part1
